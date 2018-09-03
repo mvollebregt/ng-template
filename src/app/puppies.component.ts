d69a0e5d3a69d8ebcff438431c80566f9e0c7755
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-09-03 16:41:20 +0200

    Passing in an ng-template using @ContentChild(TemplateRef) on the inner component

--- src/app/puppies.component.ts
+++ src/app/puppies.component.ts
@@ -3,13 +3,24 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'puppies-cmp',
   template: `
-    <puppy-list [puppies]="puppies"></puppy-list>
+    <puppy-list [puppies]="puppies">
+      <ng-template let-puppy>
+        <mat-list-item>
+          <img mat-list-avatar="" [src]="puppy.photo" alt="...">
+          <h3 mat-line> {{puppy.name}} </h3>
+          <p mat-line>
+            <span>Age: {{puppy.age}} </span>
+            <span>Breed: {{puppy.breed}} </span>
+          </p>
+        </mat-list-item>
+      </ng-template>
+    </puppy-list>
   `
 })
 export class PuppiesComponent {
   puppies = [
-    { name: 'Dino', age: 1, photo: '...', breed: 'Rottweiler' },
-    { name: 'Max', age: 2, photo: '...', breed: 'Beagle' },
-    { name: 'Lucy', age: 1, photo: '...', breed: 'Golden Retriever' }
+    {name: 'Dino', age: 1, photo: '...', breed: 'Rottweiler'},
+    {name: 'Max', age: 2, photo: '...', breed: 'Beagle'},
+    {name: 'Lucy', age: 1, photo: '...', breed: 'Golden Retriever'}
   ];
 }
--- src/app/puppy-list.component.ts
+++ src/app/puppy-list.component.ts
@@ -1,21 +1,17 @@
 import {Puppy} from './puppy';
-import {Component, Input} from '@angular/core';
+import {Component, ContentChild, Input, TemplateRef} from '@angular/core';
+import {NgForOfContext} from '@angular/common';
 
 @Component({
   selector: 'puppy-list',
   template: `
   <mat-list>
-    <mat-list-item *ngFor="let puppy of puppies">
-      <img mat-list-avatar="" [src]="puppy.photo" alt="...">
-      <h3 mat-line> {{puppy.name}} </h3>
-      <p mat-line>
-        <span>Age: {{puppy.age}} </span>
-        <span>Breed: {{puppy.breed}} </span>
-      </p>
-    </mat-list-item>
+    <ng-template ngFor let-puppy [ngForOf]="puppies" [ngForTemplate]="puppyTemplate">
+    </ng-template>
   </mat-list>
   `
 })
 export class PuppyListComponent {
   @Input() puppies: Puppy[];
+  @ContentChild(TemplateRef) puppyTemplate: TemplateRef<NgForOfContext<Puppy>>;
 }
